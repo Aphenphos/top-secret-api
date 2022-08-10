e0dab77110f727e9cbbf04b89141fac1149429fa
Aphenphos <MaxwillWinters@gmail.com>
2022-08-10 16:09:21 -0400

    Everything working after unsubscribe shenanigans

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -17,9 +17,10 @@ const logIn = async (userInfo = {}) => {
 
   const agent = request.agent(app);
 
-  const user = await UserService.create({ ...fakeUser, ...userInfo });
+  const user = await UserService.create({ ...fakeUser });
 
   const { email } = user;
+  console.log(email);
   await agent.post('/api/v1/users/sessions').send({ email, password });
   return [agent, user];
 };
@@ -59,7 +60,7 @@ describe('tests the user and auth', () => {
   it('tests if user is or is not an admin', async () => {
     const [agent] = await logIn();
     const res = await agent.get('/api/v1/users');
-    expect(res.status).toEqual(401);
+    expect(res.status).toEqual(403);
   });
 
   it('DELETE /sessions deletes the user session', async () => {
@@ -68,6 +69,27 @@ describe('tests the user and auth', () => {
     expect(resp.status).toBe(204);
   });
 
+  it('should make sure secret data is secret', async () => {
+    const resp = await request(app).get('/api/v1/secrets');
+  
+    expect(resp.body).toEqual({
+      message: 'You must be signed in.',
+      status: 401,
+    });
+  });
+
+  it('should make sure secrets are got properly', async () => {
+    const [agent, user] = await logIn();
+    console.log(user);
+    const resp = await agent.get('/api/v1/secrets');
+  
+    expect(resp.body).toEqual([{
+      id: expect.any(String),
+      title: 'Dont tell anyone',
+      content: 'shhh',
+      created_at: expect.any(String)
+    }]);
+  });
 
   afterAll(() => {
     pool.end();
